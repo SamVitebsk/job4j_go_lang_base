c7264f5c7d82c9f44052fbf442aa044bc33d3202
a.samusev <sam.vitebsk@gmail.com>
2026-07-22 17:30:39 +0300

    Методы

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 
-	"github.com/gofiber/fiber/v2"
-	"job4j.ru/go-lang-base/internal/api"
 	"job4j.ru/go-lang-base/internal/base"
-	"job4j.ru/go-lang-base/internal/config"
-	"job4j.ru/go-lang-base/internal/db"
-	"job4j.ru/go-lang-base/internal/repository"
 )
-
-func mainServer() {
-	ctx := context.Background()
-
-	cfg := db.Config{
-		Host:     config.Env("DB_HOST", "localhost"),
-		Port:     config.EnvInt("DB_PORT", 6543),
-		User:     config.Env("DB_USER", "postgres"),
-		Password: config.Env("DB_PASSWORD", "postgres"),
-		DBName:   config.Env("DB_NAME", "tracker"),
-		SSLMode:  config.Env("DB_SSLMODE", "disable"),
-	}
-
-	pool, err := db.NewPool(ctx, cfg.DSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pool.Close()
-
-	repo := repository.NewRepoPg(pool)
-
-	server := api.NewServer(repo)
-
-	app := fiber.New()
-	server.Route(app.Group("/api"))
-
-	err = app.Listen(":9090")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	cache := base.NewLruCache(3)
